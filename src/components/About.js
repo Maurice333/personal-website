cb06cd178f39e2120b2f02e389a1ecaed1cdc28b
Maurice333 <mauricecunningham@ymail.com>
2020-08-08 18:09:44 -0700

    before Redux

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+
+import React, {Component} from "react";
+import {hot} from "react-hot-loader"
+import "./css/main.css";
+
+class About extends Component{
+  render(){
+    return(
+      <div className="About">
+        <h1> About Me</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Projects.js
@@ -0,0 +1,18 @@
+
+import React, {Component} from "react";
+import {hot} from "react-hot-loader"
+import "./css/main.css";
+
+class Projects extends Component{
+  render(){
+    return(
+      <div className="Projects
+    ">
+        <h1> Projects
+       Me</h1>
+      </div>
+    );
+  }
+}
+
+export default Projects;
